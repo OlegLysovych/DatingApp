86209f36dd3282e4e8c8d1b02771e30c0e3bbf47
OlegLysovych <oleg.lysovych@gmail.com>
2020-07-03 19:00:34 +0300

    created AuthController and register method here

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -30,6 +30,7 @@ namespace DatingApp.API
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))); //this configuration sets at appsettings.json and calls every time when we call a class
             services.AddControllers();
             services.AddCors();
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
