d67405eea21321ffa5ef1886b7a595068e8a7595
OlegLysovych <oleg.lysovych@gmail.com>
2020-07-07 21:40:07 +0300

    end of client side login and register

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -8,24 +8,14 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
-  weatherForecasts: any;
-
+  
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.getWeatherForecasts();
   }
 
   registerToggle() {
     this.registerMode = true;
-  }
-
-  getWeatherForecasts(){
-    this.http.get('http://localhost:5000/weatherforecast').subscribe(respone =>{
-      this.weatherForecasts = respone;
-    }, error => {
-      console.log(error);
-    });
   }
 
   
