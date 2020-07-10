22aaa4bc2ea68fcf6d9cee13bf87bac47a8bcef0
OlegLysovych <oleg.lysovych@gmail.com>
2020-07-10 14:34:29 +0300

    Maked user service that wil call my api

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  baseUrl = 'http://localhost:5000/auth/';
+  baseUrl = environment.apiUrl + 'auth/';
   jwtHelper = new JwtHelperService();
   decodedToken: any;
 
@@ -18,7 +19,7 @@ export class AuthService {
       const user = respone;
       if (user) {
         localStorage.setItem('token', user.token);
-        this.decodedToken = this.jwtHelper.decodeToken(user.token);     
+        this.decodedToken = this.jwtHelper.decodeToken(user.token);
       }
     })
   );
