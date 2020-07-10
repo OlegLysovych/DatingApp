788960cfca4d08c1becef4f9de9d4f301c91d2fc
OlegLysovych <oleg.lysovych@gmail.com>
2020-07-10 18:13:05 +0300

    use auth0 for set jwt at every single request

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -4,7 +4,7 @@ import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { JwtModule } from '@auth0/angular-jwt';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -19,6 +19,10 @@ import { appRoutes } from './routes';
 import { RouterModule } from '@angular/router';
 import { UserService } from './_services/user.service';
 import { MemberCardComponent } from './members/member-card/member-card.component';
+
+export function tokenGetter(): string{
+   return localStorage.getItem('token');
+}
 
 
 @NgModule({
@@ -39,6 +43,13 @@ import { MemberCardComponent } from './members/member-card/member-card.component
       BrowserAnimationsModule,
       BsDropdownModule.forRoot(),
       RouterModule.forRoot(appRoutes),
+      JwtModule.forRoot({
+         config: {
+            tokenGetter: tokenGetter,
+            allowedDomains: ['localhost:5000'],
+            disallowedRoutes: ['localhost:5000/auth']
+         }
+      })
    ],
    providers: [
       AuthService,
