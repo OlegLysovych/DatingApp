8de8f57b55d01afa050cff0e11b1c43556512435
OlegLysovych <oleg.lysovych@gmail.com>
2020-08-05 13:44:19 +0300

    //=//

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -24,7 +24,7 @@ export class RegisterComponent implements OnInit {
   }
 
   passwordMatchValidator(g: FormGroup){
-    return g.get('password').value === g.get('confirmPassword').value ? null : {'missmatch': true};
+    return g.get('password').value === g.get('confirmPassword').value ? null : {'mismatch': true};
   }
 
   register() {
